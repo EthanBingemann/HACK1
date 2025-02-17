f8327b1a039c6e8d7b13eb9c3dcb4638e056d564
sujayf10 <sfaldu.24@gmail.com>
2025-02-16 19:03:39 -0500

    Create login.js

---
+++ login.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+const Login = ({ onFormSwitch }) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(email,password);
+}
+
+return (
+    <div className = "auth-form-container">
+        <h2>Login</h2>
+        <form className = "login-form" onSubmit = {handleSubmit}>
+            <label htmlFor = "email">Email</label>
+            <input value = {email} onChange = {(e) => setEmail(e.target.value)} type = "email" placeholder = "email@example.com" id = "email" name = "email"/>
+            <label htmlFor = "password">Password</label>
+            <input value = {password} onChange = {(e) => setPassword(e.target.value)} type = "password" placeholder = "*********" id = "password" name = "password" />
+            <button type = "submit" > Log In</button>
+        </form>
+        <button className = "link-btn" onClick={() => onFormSwitch('regular')} > Don't have an account? Click here to Register.</button>
+    </div>
+)
+}
